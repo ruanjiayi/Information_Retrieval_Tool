f367caa949f50e2f9e20b39fabbf2bec176ac49e
ruanjiayi <734801931@qq.com>
2017-06-10 16:36:32 +0800

    complete the VSM searching tool version1

---
+++ src/VSM.py
@@ -0,0 +1,142 @@
+#author:Jiayi Ruan
+#to do list:
+# 1.add spelling correction features
+# 2.adjust the output form(now we only output the title of the top articles)
+
+
+import pickle
+import math
+import indexing
+
+
+def compute_wf_idf(term_frequency, idf):
+    '''
+    :param term_frequency:the times of a term appearing in a doc
+    :type document_frequency:double
+    :param idf:inverse doucument frequency
+    :type idf:double
+    :return: weight
+    :rtype: double
+    '''
+    if term_frequency <= 0:
+        return 0.0
+    wf = 1 + math.log(term_frequency)
+    return wf*idf
+
+
+def compute_weight(one_inv_dict, N):
+    '''
+    :param one_inv_dict:one inverted index record of a specific term
+    :type one_inv_dict:dict(dict)
+    :param N:the number of all doc
+    :type N:int
+    :return: weight
+    :rtype: double
+    '''
+    #compute the total count in all doc.
+    document_frequency = len(one_inv_dict)
+    if document_frequency > 0:
+        idf = math.log(N/document_frequency)
+        for i in one_inv_dict:
+            term_frequency = 0
+            if 'title_pos' in one_inv_dict[i]:
+                term_frequency += len(one_inv_dict[i]['title_pos'])
+            if 'body_pos' in one_inv_dict[i]:
+                term_frequency += len(one_inv_dict[i]['body_pos'])
+            one_inv_dict[i]['weight'] = compute_wf_idf(term_frequency, idf)
+    else:
+        print("Error in compute_weight()")
+
+
+def get_doc_count():
+    return 10788
+
+
+def add_weight_to_index():
+    #no param no return
+    #To adjust the index with add the weight to every doc record
+    with open('../pyobjects/index.pickle', 'rb') as pickfile:
+        inv_dict = pickle.load(pickfile)
+        for i in inv_dict:
+            compute_weight(inv_dict[i], get_doc_count())
+    with open('../pyobjects/weighted_index.pickle', 'wb') as file:
+        pickle.dump(inv_dict, file)
+
+
+def compute_doc_score(term_list, inv_dict):
+    '''
+    :param term_list:the list of input query word
+    :type term_list:list[str]
+    :param inv_dict:the whole inverted index
+    :type inv_dict:dict{dict{dict}}
+    :return: doc_score
+    :rtype: dict
+    '''
+    doc_score = {}
+    for term in term_list:
+        term_dict = inv_dict[term]
+        for doc_id in term_dict:
+            if doc_id in doc_score:
+                doc_score[doc_id] += term_dict[doc_id]['weight']
+            else:
+                doc_score[doc_id] = term_dict[doc_id]['weight']
+    return doc_score
+
+
+def topK(doc_score, K):
+    '''
+    :param doc_score:The score of the doc for a given term
+    :type doc_score:dict
+    :param K:the number of the returning docs
+    :type K:int
+    :return: doc_list
+    :rtype: list[int]
+    '''
+    doc_list = sorted(doc_score, key=doc_score.get, reverse=True)
+    return doc_list[:K]
+
+
+def get_query(query, K, inv_dict):
+    '''
+    :param query:the query put in by users
+    :type query:str
+    :param K:the number of the returning docs
+    :type K:int
+    :param inv_dict:the weighted inverted index
+    :type inv_dict:dict
+    :return: doc_list
+    :rtype: list[int]
+    '''
+    term_list = indexing.get_term_list(query)
+    doc_score = compute_doc_score(term_list, inv_dict)
+    doc_list = topK(doc_score, K)
+    return doc_list
+
+
+def print_articles(doc_list):
+    '''
+    :param doc_list:the id of the doc that need to be displayed
+    :type doc_list:list
+    '''
+    path = "../Reuters/"
+    for doc_id in doc_list:
+        print(doc_id)
+        doc_path = path + str(doc_id) + ".html"
+        print
+        with open(doc_path, 'rb') as article:
+            title = article.readline()
+            print(title)
+
+
+if __name__ == "__main__":
+    #add_weight_to_index()
+    with open('../pyobjects/weighted_index.pickle', 'rb') as pickfile:
+        inv_dict = pickle.load(pickfile)
+    while(1):
+        print("Please put in the query:")
+        query = input()
+        print("Please put in the the number of how many articles you want to find:")
+        K = input()
+        print("The result is:")
+        print_articles(get_query(query, int(K), inv_dict))
+        #print(get_query(query, int(K), inv_dict))
