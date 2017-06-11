4dbf2239b3f31ce9a12f8c3becb74feaa5a03c0c
bromine96 <1477646909@qq.com>
2017-06-12 00:25:42 +0800

    spellingcorrection and synonyms
    the most basic implement
    import module: pyenchant

---
+++ src/spellcorrect_synonyms.py
@@ -0,0 +1,45 @@
+import os
+import pickle
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet as wn
+import enchant
+import indexing
+import VSM
+
+
+def spelling_correction(raw_term):
+    '''
+    :param raw_term:the term needing spelling correction
+    :type raw_term: str
+    :return: corrected term
+    :rtype: str
+    '''
+    corrector = enchant.Dict("en_US")
+    if(raw_term.strip()==""):
+        return ;
+    if(corrector.check(raw_term)==True):
+        return raw_term
+    else:
+        return corrector.suggest(raw_term)[0]
+
+def synonyms(raw_term):
+    '''
+    :param raw_term: single term to find synonyms
+    :type raw_term: str
+    :return: synonyms_list
+    :rtype: list[str]
+    '''
+    if (raw_term.strip() == ""):
+        return;
+    synonyms_list = [x.name().split(".")[0] for x in wn.synsets(raw_term)]
+    synonyms_list=sorted(set(synonyms_list), key=synonyms_list.index)
+    return synonyms_list
+
+
+# if __name__ == "__main__":
+#     with open('../pyobjects/weighted_index.pickle', 'rb') as pickfile:
+#         inv_dict = pickle.load(pickfile)
+#     while(1):
+#         term=input()
+#         print(spelling_correction(term))
+#         print(synonyms(term))
